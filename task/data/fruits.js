4cbf2279af065107f2b5fa5443113903503c2bf1
insan <insanbusted@gmail.com>
2024-12-03 23:42:57 +0700

    feat(pertemuan 9)

---
+++ task/data/fruits.js
@@ -0,0 +1,9 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+const fruits = ["Apel", "Semangka", "Jeruk"]
+
+// TODO 2: export variable fruits
+module.exports = fruits;
